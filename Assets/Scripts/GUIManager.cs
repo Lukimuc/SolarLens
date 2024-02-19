73af42671df6ef0ad6083d92fa530140a61dccc4
Markus Schmidbauer <69906431+00Markus0@users.noreply.github.com>
2024-02-19 16:18:46 +0100

    added GUIManager
    - added GUIManager for text array

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    [SerializeField] private List<String> texts = new List<String>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
