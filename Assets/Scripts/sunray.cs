59aee7628e07e9925bc257e6af38489bd131b9f9
4nn1k4 <annika.koehler21@gmail.com>
2024-02-20 10:39:47 +0100

    added sunray and overlay

---
+++ Assets/Scripts/sunray.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunray : MonoBehaviour
+{
+    public Transform sun; 
+    public Transform solarCell; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
